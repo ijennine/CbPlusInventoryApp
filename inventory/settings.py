0129a451c8fef829beaf0b98390c21b9ce2247dd
ijennine <i.jennine@lecomptoirdespharmacies.fr>
2020-11-25 16:30:06 +0100

    Set allowed host to 'cb-plus-inventory.herokuapp.com'

--- inventory/settings.py
+++ inventory/settings.py
@@ -28,7 +28,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cb-plus-inventory.herokuapp.com']
 
 
 # Application definition
